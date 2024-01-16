3320984bdb15a1486e2a2c1b68a30463cb618337
swkimmm <gksfk204@gmail.com>
2024-01-17 08:50:17 +0900

    최초 프로젝트 생성

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>네이버 OPNE API를 활용한 My Gallery 프로젝트 </h1>
+    </>
+  )
+}
+
+export default App
